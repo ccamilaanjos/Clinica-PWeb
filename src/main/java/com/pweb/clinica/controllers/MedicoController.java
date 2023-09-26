94cc28cf7c6e74070aab5169d32600bf9cd20b29
Camila Anjos <anjoscam.irl@gmail.com>
2023-09-25 22:48:13 -0300

    Remover funcionando nos dois controllers

--- src/main/java/com/pweb/clinica/controllers/MedicoController.java
+++ src/main/java/com/pweb/clinica/controllers/MedicoController.java
@@ -7,18 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pweb.clinica.dtos.MedicoDTO;
 import com.pweb.clinica.dtos.MedicoFormDTO;
-import com.pweb.clinica.dtos.PacienteDTO;
-import com.pweb.clinica.dtos.PacienteFormDTO;
 import com.pweb.clinica.models.Medico;
-import com.pweb.clinica.models.Paciente;
 import com.pweb.clinica.models.Pessoa;
 import com.pweb.clinica.services.MedicoService;
 
@@ -46,9 +43,9 @@ public class MedicoController implements PessoaController<MedicoFormDTO> {
 		return null;
 	}
 
-	@DeleteMapping("/{id}")
+	@DeleteMapping("/")
 	@Override
-	public ResponseEntity<?> remover(@PathVariable Long id) {
+	public ResponseEntity<?> remover(@RequestParam("id") Long id) {
 		if(this.medicoService.tornarInativo(id) == null) {
 			return new ResponseEntity<>("Registro não encontrado", HttpStatus.NOT_FOUND);
 		}
