b53f5d155f264f2a501eddd28d38658d2b569403
Camila Anjos <anjoscam.irl@gmail.com>
2023-09-25 23:55:09 -0300

    Annotations

--- src/main/java/com/pweb/clinica/repositories/MedicoRepository.java
+++ src/main/java/com/pweb/clinica/repositories/MedicoRepository.java
@@ -1,7 +1,9 @@
 package com.pweb.clinica.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pweb.clinica.models.Medico;
 
+@Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {}
--- src/main/java/com/pweb/clinica/repositories/PacienteRepository.java
+++ src/main/java/com/pweb/clinica/repositories/PacienteRepository.java
@@ -1,7 +1,9 @@
 package com.pweb.clinica.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pweb.clinica.models.Paciente;
 
+@Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long>{}
