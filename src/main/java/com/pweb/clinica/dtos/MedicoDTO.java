331c83487ac2f4f6c93918f1c660dd3595433d14
Camila Anjos <anjoscam.irl@gmail.com>
2023-10-03 21:23:36 -0300

    Imports removidos

--- src/main/java/com/pweb/clinica/dtos/MedicoDTO.java
+++ src/main/java/com/pweb/clinica/dtos/MedicoDTO.java
@@ -1,7 +1,6 @@
 package com.pweb.clinica.dtos;
 
 import com.pweb.clinica.models.Medico;
-import com.pweb.clinica.utils.converters.EnderecoConverter;
 
 public record MedicoDTO(Long id, String nome, String email, String telefone, EnderecoDTO endereco, String crm,
 		EspecialidadeDTO especialidade, Boolean ativo) {
--- src/main/java/com/pweb/clinica/services/PessoaService.java
+++ src/main/java/com/pweb/clinica/services/PessoaService.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.pweb.clinica.models.Pessoa;
-
 public interface PessoaService<Model, PostDTO, PutDTO, DTO> {
 	public Page<DTO> getPagina(Pageable pageable);
 	public DTO cadastrar(PostDTO formDto) throws Exception;
