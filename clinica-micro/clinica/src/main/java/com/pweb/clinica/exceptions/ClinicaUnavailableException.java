407a59b10a31d232132126dc8addf2447bc09066
Camila Anjos <anjoscam.irl@gmail.com>
2023-10-14 04:51:00 -0300

    Removendo dir de apoio

--- clinica-micro/clinica/src/main/java/com/pweb/clinica/exceptions/ClinicaUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package com.pweb.clinica.exceptions;
-
-public class ClinicaUnavailableException extends Exception {
-	
-	public ClinicaUnavailableException() {
-		super("A clínica não está disponível para marcações no momento informado");
-	}
-}
